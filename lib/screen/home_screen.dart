87c43ba270f8df1002a47dbdf94a73acbe492262
Haruachm <horang_e7@naver.com>
2023-02-07 17:20:54 +0900

    Setting Webview 3.0.0 version

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: 'https://parkjh7764.tistory.com',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
